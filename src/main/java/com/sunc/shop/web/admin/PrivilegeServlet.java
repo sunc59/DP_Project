cc94104e6520f1bf36adaf68b718958374eabb85
suncBit <769987197@qq.com>
2020-06-19 21:11:58 +0800

    后台权限管理

---
+++ src/main/java/com/sunc/shop/web/admin/PrivilegeServlet.java
@@ -0,0 +1,18 @@
+package com.sunc.shop.web.admin;
+
+import com.sunc.shop.web.servlet.BaseServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * @auther sunc
+ * @date 2020/6/11 22:12
+ */
+@WebServlet("/admin/privilege/*")
+public class PrivilegeServlet extends BaseServlet {
+
+    /**
+     *  怎么处理权限未定，先搁置
+     */
+
+}
